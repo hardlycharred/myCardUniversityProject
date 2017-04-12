de34acf00d696cf62a69d5eadf67d410f6cf2c7a
Charlie Hard <harch388@student.otago.ac.nz>
2017-04-12 15:20:53 +1200

    Changed the login icon file to a key

--- app/src/main/java/com/example/charliehard/gobus/LoginActivity.java
+++ app/src/main/java/com/example/charliehard/gobus/LoginActivity.java
@@ -6,6 +6,7 @@ import android.annotation.TargetApi;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.app.LoaderManager.LoaderCallbacks;

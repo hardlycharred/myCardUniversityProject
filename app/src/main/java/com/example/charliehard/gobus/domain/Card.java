07967a918381b2279f3c28234c80cebaa6da8a57
Emily <mccem568@student.otago.ac.nz>
2017-05-18 13:33:51 +1200

    Domain classes added to Git

---
+++ app/src/main/java/com/example/charliehard/gobus/domain/Card.java
@@ -0,0 +1,45 @@
+package com.example.charliehard.gobus.domain;
+
+import java.io.Serializable;
+
+/**
+ * Created by Charlie Hard on 1/05/2017.
+ */
+
+public class Card implements Serializable {
+
+    private String cardNumber;
+    private Double balance;
+
+    public Card(String cardNumber, Double balance) {
+        this.cardNumber = cardNumber;
+        this.balance = balance;
+    }
+
+    public Card() {
+    }
+
+    public String getCardNumber() {
+        return cardNumber;
+    }
+
+    public void setCardNumber(String cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+
+    public Double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(Double balance) {
+        this.balance = balance;
+    }
+
+    @Override
+    public String toString() {
+        return "Card{" +
+                "cardNumber='" + cardNumber + '\'' +
+                ", balance=" + balance +
+                '}';
+    }
+}

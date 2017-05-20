f23e1881dbfeb34a52a81a2a717327c35ec8bc00
emilystrang <strem590@student.otago.ac.nz>
2017-05-20 18:12:01 +1200

    Top Up Activity Layout added. Design Completed. Currently no back button. Home screen activity updated to update different text fields. Text edit style added.

--- app/src/main/java/com/example/charliehard/gobus/gui/HomeScreenActivity.java
+++ app/src/main/java/com/example/charliehard/gobus/gui/HomeScreenActivity.java
@@ -62,7 +62,7 @@ public class HomeScreenActivity extends AppCompatActivity {
         if (curCustomer != null && mTextMessage != null) {
             setContentView(R.layout.activity_home_screen_logged_in);
             mTextMessage = (TextView) findViewById(R.id.textView3);
-            mTextMessage.setText("Hello, " + curCustomer.getFirstName() + " Your balance is $" + card.getBalance());
+            mTextMessage.setText("Hello, " + curCustomer.getFirstName() );
             mTextMessage = (TextView) findViewById(R.id.textView5);
             mTextMessage.setText("$" + card.getBalance() + " ");
             //mTextMessage.setText("Transaction1" + trans1.getDate() + " " + trans1.getTime() + " " + trans1.getAmount());
